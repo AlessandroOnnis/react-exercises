0c87d8fc404809178b7ca280e45bfb047827cc7e
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-09-29 01:58:18 +0200

    components-03

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Hello } from './components/hello';
+
+class App extends React.Component {
+  render() {
+    return (
+    <div>
+      <Hello/>
+    </div>
+    )
+  };
+}
+export default App;
