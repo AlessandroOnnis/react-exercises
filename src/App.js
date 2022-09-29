24fa1b326385f9fd64c032c4bfd0b2725025ab10
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-09-29 04:41:31 +0200

    props-02

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     return (
     <div>
       <Hello/>
-      <Welcome name="Batman"/>
+      <Welcome name=""/>
     </div>
     )
   };
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,7 +1,44 @@
 import React from "react"
 
+//===Esecuzione esercizio===setting default value
 export class Welcome extends React.Component{
     render(){
-        return <p>Welcome, {this.props.name}!</p>
+        return <p>Welcome, {this.props.name?this.props.name: 'nome di default'}!</p>
     }
 }
+//RISPOSTE
+//se l'attributo non viene assegnato non viene stampato il valore di this.props.name perché non esiste.
+//se manca il valore dell'attributo si può impostare il suo valore di default, 
+//Nel caso non esista l'attributo su App.js, possiamo assegnarlo di default(riga 28).
+
+//===Nota Personale===setting default attribute(non deve essere assegnato dentro app.js)
+//Devo ricordarmi potrebbe essermi utile in futuro con alcuni componenti
+// export class Welcome extends React.Component {
+//     render() {
+//         return (
+//             <>{
+//                 this.props.name ?
+//                     <p>Welcome, {this.props.name}!</p>
+//                     : <p>Welcome, {Welcome.defaultProps.name}!</p>
+//             }
+//             </>
+//         )
+//     }
+// }
+
+// Welcome.defaultProps = {
+//     name: "chiunque tu sia",
+// }
+
+//===Nota Personale===setting default string if attribute it's missing
+// export class Welcome extends React.Component{
+//     render(){
+//         return(
+//         <>
+//          {this.props.name?
+//          <p>Welcome, {this.props.name}!</p>
+//          :<p>Welcome, chiunque tu sia!</p>}
+//         </>
+//         )
+//     }
+// }
