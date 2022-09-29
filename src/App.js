91cdcbdfb4e47e71732a624c79cfcb07f4d87dca
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-09-29 03:58:09 +0200

    props-01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Hello } from './components/hello';
+import { Welcome } from './components/welcome';
 
 class App extends React.Component {
   render() {
     return (
     <div>
       <Hello/>
+      <Welcome name="Batman"/>
     </div>
     )
   };
