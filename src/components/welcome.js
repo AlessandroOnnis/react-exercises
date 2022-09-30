ac491ce43693dc03411daea9cdeecd4b9b0d1fc0
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-09-30 15:09:54 +0200

    conditional-rendering-01

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -3,12 +3,21 @@ import React from "react"
 //===Esecuzione esercizio===setting default value
 export class Welcome extends React.Component{
     render(){
-        return (
-        <>
-            <p>Welcome, {<strong>{this.props.name?this.props.name: 'nome di default'}</strong>}!</p>
-            <Age age={this.props.age}/>
-        </>
-        )
+        const maggiorenne = this.props.age > 18;
+        if(maggiorenne){
+            return (
+            <>
+                <p>Welcome, {<strong>{this.props.name?this.props.name: 'nome di default'}</strong>}!</p>
+                <Age age={this.props.age}/>
+            </>
+            )
+        }else{
+            return (
+            <>
+                <p>Welcome, {<strong>{this.props.name?this.props.name: 'nome di default'}</strong>}!</p>
+            </>
+            )
+        }
     }
 }
 function Age(props) {
