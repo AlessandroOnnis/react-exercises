03735a36f72e37e072e26f00effbce16af4d1d64
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-09-30 16:06:04 +0200

    conditional-rendering-05

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -3,24 +3,27 @@ import React from "react"
 //===Esecuzione esercizio===setting default value
 export class Welcome extends React.Component{
     render(){
-        const ageRange = this.props.age > 18 && this.props.age <65;
+        const ageRange = this.props.age > 18 && this.props.age <65 ||  this.props.age < 65;
         const person = this.props.name === 'Jhon';
+        let age;
         if(this.props.age && ageRange && person){
-            return (
-            <>
-                <p>Welcome, {<strong>{this.props.name?this.props.name: 'nome di default'}</strong>}!</p>
-                <Age age={this.props.age}/>
-            </>
-            )
-        }else{
-            return (
-            <>
-                <p>Welcome, {<strong>{this.props.name?this.props.name: 'nome di default'}</strong>}!</p>
-            </>
-            )
+            age = <Age age={this.props.age}/>
         }
+        return (
+        <>
+            <p>Welcome, {<strong>{this.props.name?this.props.name: 'nome di default'}</strong>}!</p>
+            {age}
+        </>
+        )
     }
 }
 function Age(props) {
-    return <p>Your age is, {props.age}</p>
+    let ok = props.age >= 18;
+        if(ok){
+            let message = "Your age is,"
+            return <>{message} {props.age}</>
+        }if(!ok){
+            let message = "You're to young"
+            return <>{message}</>
+        }
 }
