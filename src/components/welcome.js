f6f6853d077ba284c6772467c45b742e31bb8590
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-11 13:52:53 +0200

    fix: props-05 component Age

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,25 +1,14 @@
 import React from "react"
+import { Age } from "./age"
 
 //===Esecuzione esercizio===setting default value
 export class Welcome extends React.Component{
     render(){
         return (
         <>
-            <p>Welcome, {<strong>{this.props.name?this.props.name: 'nome di default'}</strong>}!</p>
+            <p>Welcome, {this.props.name?this.props.name: 'nome di default'}!</p>
             <Age age={this.props.age}/>
-            <p>{AgeDue(this.props.age)} </p>
         </>
         )
     }
 }
-function Age(props) {
-    return <p>Your age is, {props.age}</p>
-}
-
-//quale dei due conviene?
-//===Esercizio Props-05====Metodo due 
-function AgeDue(props){
-    return <>Your age is, {props}</>
-}
-
-//========
