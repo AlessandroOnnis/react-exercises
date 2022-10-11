40d7905461ab6f3013c6dc1db9915c6c3fbf6580
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-11 18:56:02 +0200

    fix: conditional-rendering-04

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Hello } from './components/hello';
 import { Welcome } from './components/welcome';
-const user = 'Giorgio'
 
 class App extends React.Component {
   render() {
     return (
     <div>
       <Hello/>
-      <Welcome name={user} age="18"/>
+      <Welcome name={'John'} age={18}/>
     </div>
     )
   };
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,26 +1,14 @@
 import React from "react"
+import { Age } from "./age"
 
 //===Esecuzione esercizio===setting default value
-export class Welcome extends React.Component{
-    render(){
-        const ageRange = this.props.age > 18 && this.props.age <65;
-        const person = this.props.name === 'Jhon';
-        if(this.props.age && ageRange && person){
-            return (
+export class Welcome extends React.Component {
+    render() {
+        return (
             <>
-                <p>Welcome, {<strong>{this.props.name?this.props.name: 'nome di default'}</strong>}!</p>
-                <Age age={this.props.age}/>
+                <p>Welcome, <strong>{this.props.name ? this.props.name : 'nome di default'}</strong>!</p>
+                {this.props.age >= 18 && this.props.age <= 65 && this.props.name === 'John' && <Age age={this.props.age} />}
             </>
-            )
-        }else{
-            return (
-            <>
-                <p>Welcome, {<strong>{this.props.name?this.props.name: 'nome di default'}</strong>}!</p>
-            </>
-            )
-        }
+        )
     }
-}
-function Age(props) {
-    return <p>Your age is, {props.age}</p>
 }
