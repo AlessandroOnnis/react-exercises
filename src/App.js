56cb6732db25c9e705875548f9c67ddaa396ba2f
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-11 19:02:32 +0200

    fix: conditional-rendering-05

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Hello } from './components/hello';
 import { Welcome } from './components/welcome';
-const user = 'Jhon'
 
 class App extends React.Component {
   render() {
     return (
     <div>
       <Hello/>
-      <Welcome name={user} age="20"/>
+      <Welcome name={'John'} age={18}/>
     </div>
     )
   };
