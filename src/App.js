db72c91b1738d4d3a43f5be01e09ee3a462b531d
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-11 22:45:20 +0200

    chore: forms-01

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react';
+import { Hello } from './components/hello';
+import { Counter } from './components/counter';
 import { ClickCounter } from './components/clickCounter';
 import { ClickTracker } from './components/clickTracker';
-import { Counter } from './components/counter';
-import { Hello } from './components/hello';
-// import { Welcome } from './components/welcome';
 import { InteractiveWelcome } from './components/interactiveWelcome';
-// const user = 'Jhon'
 
 class App extends React.Component {
   render() {
     return (
     <div>
       <Hello/>
-      {/* <Welcome name={user} age={20}/> */}
       <Counter start={0} timing={1000} add={1}/>
       <ClickCounter />
       <ClickTracker />
-      <InteractiveWelcome/>
+      <InteractiveWelcome />
     </div>
     )
   };
--- src/components/clickCounter.js
+++ src/components/clickCounter.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { CounterButton } from "./counterButton";
 
 export class ClickCounter extends React.Component {
     constructor(props) {
         super(props)
         this.state = {amount: 0}
     }
-    userInteraction = ((ev)=>{
+    userInteraction = (()=>{
         this.setState({amount: this.state.amount + 1})
     })
 
@@ -13,12 +14,8 @@ export class ClickCounter extends React.Component {
         return (
             <div>
                 <h2>Count : {this.state.amount}</h2>
-                <CounterButton add={this.userInteraction}/>
+                <CounterButton name="plus" evento={this.userInteraction}/>
             </div>
         );
     }
-}
-
-function CounterButton(props){
-    return <button onClick={props.add}>Plus</button>
 }
