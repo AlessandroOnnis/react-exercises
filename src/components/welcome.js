35d882a86b467459d4aa704fcfc7f43e6bc6e6a5
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-11 18:43:52 +0200

    fix: conditional-rendering-02

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,28 +1,14 @@
 import React from "react"
+import { Age } from "./age"
 
 //===Esecuzione esercizio===setting default value
-export class Welcome extends React.Component{
-    render(){
-        const maggiorenne = this.props.age > 18;
-        if(maggiorenne){
-            return (
+export class Welcome extends React.Component {
+    render() {
+        return (
             <>
-                <p>Welcome, {<strong>{this.props.name?this.props.name: 'nome di default'}</strong>}!</p>
-                <Age age={this.props.age}/>
+                <p>Welcome, {this.props.name ? this.props.name : 'nome di default'}!</p>
+                {this.props.age >= 18 && <Age age={this.props.age} />}
             </>
-            )
-        }else{
-            return (
-            <>
-                <p>Welcome, {<strong>{this.props.name?this.props.name: 'nome di default'}</strong>}!</p>
-            </>
-            )
-        }
-    }
-}
-//per me sarebbe più corretto così, controllare sull'elemento se esiste, poi passare i controlli sull'altro
-function Age(props) {
-    if(props.age){
-        return <p>Your age is, {props.age}</p>
+        )
     }
 }
