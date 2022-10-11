54fe67ecb6ed859134e8c6494264778543a9b850
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-11 19:24:21 +0200

    fix: state-01

--- src/components/counter.js
+++ src/components/counter.js
@@ -14,3 +14,20 @@ export class Counter extends React.Component {
         return <h1>{this.state.seconds}</h1>
     }
 }
+
+
+//versione moderna?
+
+// import {useEffect, useState} from "react";
+
+// export function Counter(){
+//     const[counter, setCounter] = useState(0);
+
+//     useEffect(()=>{
+//             setTimeout(()=> setCounter(counter +1), 1000)
+        
+//     }, [counter])
+//     return(
+//         <h1>{counter}</h1>
+//     )
+// }
