c46449f02943bca38f055f4685583f6f1c1da39c
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-11 19:53:27 +0200

    fix: component-lifecycle-02

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { CounterDisplay } from "./counterDisplay";
 
 export class Counter extends React.Component {
     constructor(props) {
@@ -15,14 +16,8 @@ export class Counter extends React.Component {
             clearInterval(this._interval)
         }
     }
+
     render() {
-        return (
-            <>
-                <CounterDisplay state={this.state.seconds}/>
-            </>
-        )
+        return <CounterDisplay state={this.state.seconds}/>
     }
-}
-function CounterDisplay(props){
-    return <h1>{props.state}</h1>
 }
