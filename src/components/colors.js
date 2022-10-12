a0947dc892583ad0e13dc5567950f65d79624a78
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-12 13:50:31 +0200

    lists-02

--- src/components/colors.js
+++ src/components/colors.js
@@ -5,8 +5,12 @@ export class Colors extends React.Component {
     render() {
         return( 
             <ul>
-                {this.props.colors.map((item) => (<li>{item}</li>))}
+                {this.props.colors.map((item) => (<li key={item[0]}>{item}</li>))}
             </ul>
         )
     }
 }
+
+
+//dato che lists-01 e 02 sono esterni agli esercizi da consegnare ho fatto un po' di testa mai,
+//avevo fatto sul clickTracker l'object per passare id e key qui ho semplificato giusto per completare la task
