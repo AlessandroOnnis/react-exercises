6b4ecf6409a53e834161105fe4cbd5113f9453b9
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-14 07:43:46 +0200

    styling-components-02

--- src/components/generalButton.js
+++ src/components/generalButton.js
@@ -2,6 +2,14 @@ import React from "react";
 
 export class GeneralButton extends React.Component {
     render() {
-        return <button name={this.props.name} onClick={this.props.evento} type={this.props.type} disabled={this.props.disabled}>{this.props.name}</button>
+        return (
+        <button 
+            name={this.props.name} 
+            onClick={this.props.evento} 
+            type={this.props.type} 
+            disabled={this.props.disabled} 
+            style={this.props.style}
+            >{this.props.name}</button>
+        )
     }
 }
--- src/components/login.js
+++ src/components/login.js
@@ -31,12 +31,15 @@ export class Login extends React.Component {
     }
 
     render() {
+        const buttonLogin = {
+            backgroundColor: this.state.password.length < 8? 'red':'green'
+        }
         return (
             <div>
                 <input name='username' placeholder="username" value={this.state.username} onChange={this.info} />
                 <input name='password' placeholder="password" type='password' value={this.state.password} onChange={this.info} />
                 <input name='checkbox' type='checkbox' checked={this.state.checkbox} onChange={this.info} />
-                <GeneralButton name="login" type="button" evento={this.onLogin} disabled={this.state.disabled}/>
+                <GeneralButton name="login" type="button" evento={this.onLogin} disabled={this.state.disabled} style={buttonLogin}/>
                 <GeneralButton name="Reset" type="button" evento={this.onReset} disabled={this.state.disabled}/>
             </div>
         )
