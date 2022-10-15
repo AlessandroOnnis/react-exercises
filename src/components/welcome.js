6ad8b1de2d99b367158b6a8293e676a4f12a713a
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-15 16:22:17 +0200

    function-components-01

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,13 +1,11 @@
-import React, { Fragment } from "react"
+import React from "react"
 import { Age } from "./age"
 
-export class Welcome extends React.Component {
-    render() {
-        return (
-            <>
-                <p className ='welcome'>Welcome, <strong>{this.props.name ? this.props.name : 'nome di default'}</strong>!</p>
-                {this.props.age <= 65 && this.props.name === 'John' && <Age age={this.props.age} />}
-            </>
-        )
-    }
+export function Welcome(props){
+    return(
+        <>
+            <p className ='welcome'>Welcome, <strong>{props.name ? props.name : 'nome di default'}</strong>!</p>
+            {props.age <= 65 && props.name === 'John' && <Age age={props.age} />}
+        </>
+    )
 }
