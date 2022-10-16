93a4ce9671022edd4288b4c07a64db2c11889f91
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-16 11:21:00 +0200

    useState-02

--- src/components/login.js
+++ src/components/login.js
@@ -1,45 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import { GeneralButton } from "./generalButton";
 
-export class Login extends React.Component {
-    state = {
+export function Login(){
+    const[userData, setUserData] = useState({
         username: '',
         password: '',
         checkbox: false,
         disabled: true
-    }
-    info = (e) => {
-        let userValue = e.target.value;
-        let userName = e.target.name
-        let userType = e.target.type
-        let userChecked = e.target.checked
-        this.setState({
-            [userName]: userType === 'checkbox' ? userChecked : userValue
-        })
-        this.setState((state)=>{
-            return {disabled: !state.username || !state.password}
+    })
+    function info(e){
+        const {name, value, type, checked} = e.target
+        setUserData(userData=>{
+            return {
+                ...userData,
+                [name]: type === 'checkbox'?checked: value
+            }
         })
     }
-    onLogin= ()=>{ this.props.onLogin(this.state) }
-    onReset = ()=>{
-        this.setState({
-            username:'',
-            password:'',
-            checkbox:false,
-            disabled:true
+    function onLogin(){
+        setUserData((userData) => {return {...userData}})
+        console.log(userData)
+    }
+    function onReset(){
+        setUserData((userData) => {
+            return {
+                ...userData,
+                username: '',
+                password: '',
+                checkbox: false,
+                disabled: true
+            }
         })
     }
-
-    render() {
-        const buttonLogin = {backgroundColor: this.state.password.length < 8? 'red':'green'}
-        return (
-            <div>
-                <input name='username' placeholder="username" value={this.state.username} onChange={this.info} />
-                <input name='password' placeholder="password" type='password' value={this.state.password} onChange={this.info} />
-                <input name='checkbox' type='checkbox' checked={this.state.checkbox} onChange={this.info} />
-                <GeneralButton name="login" type="button" evento={this.onLogin} disabled={this.state.disabled} style={buttonLogin}/>
-                <GeneralButton name="Reset" type="button" evento={this.onReset} disabled={this.state.disabled}/>
-            </div>
-        )
-    }
+    const buttonLogin = {backgroundColor: userData.password.length < 8? 'red':'green'}
+    return (
+                
+        <div>
+            <input name='username' onChange={info} type='username' value={userData.username} placeholder="username"/>
+            <input name='password' onChange={info} type='password' value={userData.password} placeholder="password"/>
+            <input name='checkbox' onChange={info} type='checkbox' checked={userData.checkbox}/>
+            <GeneralButton name="login" type="button" evento={onLogin} disabled={!userData.username || !userData.password} style={buttonLogin}/>
+            <GeneralButton name="Reset" type="button" evento={onReset} disabled={!userData.username || !userData.password}/>
+        </div>
+    )
 }
