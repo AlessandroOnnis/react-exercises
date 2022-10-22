5b9eec1b9515f26398dbccbe0f739dc664096d46
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-22 15:13:06 +0200

    react-router-01

---
+++ src/AppOld.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Hello } from './components/hello';
+import { Counter } from './components/counter';
+import { ClickCounter } from './components/clickCounter';
+import { ClickTracker } from './components/clickTracker';
+import { InteractiveWelcome } from './components/interactiveWelcome';
+import { Login } from './components/login';
+import { UncontrolledLogin } from './components/uncontrolledLogin';
+import { Colors } from './components/colors';
+import { TodoList } from './components/toDoList';
+import { Container } from './components/container';
+import { GeneralButton } from './components/generalButton';
+import { Sum } from './components/sum';
+import { GithubUserList } from './components/GithubUserList';
+import { FormControlledTracked } from './components/formControlledTracked';
+import { CarDetails } from './components/carDetails';
+import { FilteredList } from './components/filteredList';
+import { studenti } from './components/listaStudenti';
+
+export class AppOld extends React.Component {
+    render() {
+        return (
+            <Container title="Random title in a random App">
+                <Hello />
+                <Counter start={0} timing={1000} add={1} />
+                <ClickCounter onCounterChange={(c=> console.log(c))}/>
+                <ClickTracker />
+                <InteractiveWelcome />
+                <Login onLogin={(x) => { console.log(x) }} />
+                <UncontrolledLogin onLogin={(x) => { console.log(x) }} />
+                <Colors colors={['red', 'yellow', 'green']} />
+                <TodoList>{(i, remove) => {
+                    const task = i.map((item, index) =>
+                        <li key={index}>
+                            <GeneralButton evento={() => { remove(index) }} name={'remove'} key={index} />
+                            {item}
+                        </li>)
+                    return task &&<ul>{task}</ul>}}
+                </TodoList>
+                <GithubUserList/>
+                <FormControlledTracked/>
+                <CarDetails/>
+                <Sum />
+                <FilteredList persone={studenti}/>
+            </Container>
+        )
+    };
+}
