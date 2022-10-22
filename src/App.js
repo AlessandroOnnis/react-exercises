dab0046e16659b2f578c66415726444beaa88b06
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-22 16:27:30 +0200

    react-router-02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {Routes, Route} from 'react-router-dom'
 import { Welcome } from './components/welcome'
+import { ClickCounter } from './components/clickCounter'
 
 export function App(){
 
     return(
     <Routes>
         <Route path="/" element={<Welcome name={'Alfio'} />} />
+        <Route path="counter" element={<ClickCounter/>} />
     </Routes>
     )
 }
