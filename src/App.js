b812f970335f372f862dd7099e4ccd9d15230801
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-23 20:04:53 +0200

    react-router-05

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import { Routes, Route, Link } from 'react-router-dom'
 import { Welcome } from './components/welcome'
 import { ClickCounter } from './components/clickCounter'
 import { ShowGithubUser } from './components/ShowGithubUser'
-import React from 'react'
 
 export function App() {
 
-    const navigation={
+    const navigation = {
         color: 'white',
         border: '1px solid black',
         padding: 5,
         background: '#ff8214',
         borderRadius: 5,
     }
+    const navigation2 = {
+        color: 'white',
+        border: '1px solid black',
+        padding: 5,
+        background: 'red',
+        borderRadius: 5,
+    }
     return (
         <>
-            <div style={{display: 'flex', justifyContent: 'center', padding: 10, margin: 5, gap: 10}}>
+            <div style={{ display: 'flex', justifyContent: 'center', padding: 10, margin: 5, gap: 10 }}>
                 <Link style={navigation} to={'/'}>Welcome</Link>
                 <Link style={navigation} to={'counter'}>ClickCounter</Link>
                 <Link style={navigation} to={'users/ddd'}>GithubUser</Link>
@@ -24,6 +30,12 @@ export function App() {
                 <Route path="/" element={<Welcome name={'Alfio'} />} />
                 <Route path="counter" element={<ClickCounter />} />
                 <Route path="users/:username" element={<ShowGithubUser />} />
+                <Route path="*" element={// la pagina users non esiste ho testato su quella
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>Page not found
+                        <Link to={'/'}>
+                            <div style={navigation2}>Go back to Welcome</div>
+                        </Link>
+                    </div>} />
             </Routes>
         </>
     )
