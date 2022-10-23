6b13778c1e1510b00db5f0b640c2aed56ac7b06c
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-23 19:45:02 +0200

    react-router-04

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { Welcome } from './components/welcome'
 import { ClickCounter } from './components/clickCounter'
 import { ShowGithubUser } from './components/ShowGithubUser'
+import React from 'react'
 
-export function App(){
+export function App() {
 
-    return(
-    <Routes>
-        <Route path="/" element={<Welcome name={'Alfio'} />} />
-        <Route path="counter" element={<ClickCounter/>} />
-        <Route path="users/:username" element={<ShowGithubUser/>} />
-    </Routes>
+    const navigation={
+        color: 'white',
+        border: '1px solid black',
+        padding: 5,
+        background: '#ff8214',
+        borderRadius: 5,
+    }
+    return (
+        <>
+            <div style={{display: 'flex', justifyContent: 'center', padding: 10, margin: 5, gap: 10}}>
+                <Link style={navigation} to={'/'}>Welcome</Link>
+                <Link style={navigation} to={'counter'}>ClickCounter</Link>
+                <Link style={navigation} to={'users/ddd'}>GithubUser</Link>
+            </div>
+            <Routes>
+                <Route path="/" element={<Welcome name={'Alfio'} />} />
+                <Route path="counter" element={<ClickCounter />} />
+                <Route path="users/:username" element={<ShowGithubUser />} />
+            </Routes>
+        </>
     )
 }
