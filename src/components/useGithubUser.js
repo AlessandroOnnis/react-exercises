fcb7cc175fde346be1ba86fef1b30e41877bb44b
AlessandroOnnis <alessandroonnis43@gmail.com>
2022-10-25 16:35:25 +0200

    swr-02

--- src/components/useGithubUser.js
+++ src/components/useGithubUser.js
@@ -4,7 +4,7 @@ import useSWR from "swr"
 const fetchUser = (url) => fetch(url).then((response) => response.json())
 
 export function useGithubUser(username){
-    const {data, error} = useSWR(`https://api.github.com/users/${username}`, fetchUser)
+    const {data, error} = useSWR( username !== null && `https://api.github.com/users/${username}`, fetchUser)
 
     return{
         data,
@@ -12,7 +12,4 @@ export function useGithubUser(username){
         error,
     }
 }
-
-//ho perso due ore a capire perché non funzionasse, ho controllato tutti i componenti che interagivano con lo username,
-//ho provato a destrutturare, a correggere parentesi a destra e manca, controllare in console che dati arrivassero,
-//e nulla, dopo due ore ho imparato che data è una parola obbligatoria nell'swr, avevo userData dal precedente esercizio e non printava
+//facevo già il controllo dell'username su githubUserList che ha più senso visto che il nome non entra proprio in circolo
